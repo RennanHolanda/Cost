bf36cacbfdc713b74eeda6b410065742b6450f33
Bruno Rennan <bruno_rennan@hotmail.com>
2022-10-29 10:00:25 -0300

    Inserindo dados no banco via Api

--- src/App.js
+++ src/App.js
@@ -2,31 +2,29 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
-import NewProject from "./components/pages/NewProject";
-import Header from './components/pages/Index';
+
 
 import Container from './components/layout/Container';
-
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Projects from './components/pages/Projects';
+import NewProject from './components/pages/NewProject';
 
 function App() {
   return (
-    
-    <Container customClass="min-heigth">
           <Router>
-            <Header />
-              <Routes>
-                <Route exact path='/' element={<Home />}/>
-                <Route path='/company' element={<Company />}/>
-                <Route path='/contact' element={<Contact />}/>
-                <Route path='/newproject' element={<NewProject />}/>
-              </Routes>
-            </Router>
-            <p>Footer</p>
-        </Container>
-  
-   
-
-    
+             <Navbar />
+              <Container customClass="min-height">
+                  <Routes >
+                    <Route exact path='/' element={<Home />}/>
+                    <Route path='/company' element={<Company />}/>
+                    <Route path='/contact' element={<Contact />}/>
+                    <Route path='/project' element={<Projects />}/>
+                    <Route path='/newproject' element={<NewProject />}/>
+                  </Routes>
+              </Container>
+            <Footer />
+          </Router>
   );
 }
 
--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,5 +1,50 @@
+import { useNavigate } from 'react-router-dom';
+
+import ProjectForm from '../project/ProjectForm';
+import styles from './NewProject.module.css'
+
 function NewProject() {
-    return <h1>NewProject</h1>
+
+    const history = useNavigate()
+
+    async function createPost(project) {
+        const config = {
+            method: 'POST',
+            body: JSON.stringify(project)
+        }
+        const response = await fetch('http://localhost:5000/projects', config)
+        const data = await response.json()
+        console.log(data)
+        alert('Prjeto eviando com sucesso')
+        history('/project')
+
+
+        
+        // initialize cost ans services
+        // project.cost = 0
+        // project.services = []
+        // fetch('http://localhost:5000/projects', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(project)
+        // }).then((resp => resp.json())
+        // .then((data) => {
+        //     console.log(data)
+        //     //redirect
+        //     history.push('/project', {message: 'Projeto criado om sucesso!'})
+        // })
+        // ).catch(err => console.log(err))
+    };
+
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm handLeSubmit={createPost} btnText="Criar projeto"/>
+        </div>
+    )
 };
 
 export default NewProject;
